58a8f044e7c40e074498568aa7d2acc11d7a7be1
VIK <vikuvsh95@gmail.com>
2020-04-15 19:25:57 +0300

    New commit

---
+++ sc.js
@@ -0,0 +1,10 @@
+var arr = ['1.jpg', '2.jpg', '3.bmp'];
+// var inp = prompt('Enter the variant 1, 2, 3: ');
+// alert('Your item is ' + arr[(inp-1)]);
+var a = confirm('Confirm it');
+if (a == true){
+    alert('OK');
+}
+else{
+    alert('Why');
+}
