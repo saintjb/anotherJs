e51feaa965c30c3cb2ca975771079a2ace9d6ff0
VIK <vikuvsh95@gmail.com>
2020-04-25 23:40:21 +0300

    new work

--- .vscode/less2/scrip.js
+++ .vscode/less2/scrip.js
@@ -1,20 +1,45 @@
-window.addEventListener('DOMContentLoaded', function(){
-    let box = document.getElementById('btn');
-    // box.addEventListener('touchstart', function(event){
-    //     event.preventDefault();
-    //     console.log('Button touchstart');
-    //     console.log(event.changedTouches);
-    //     console.log(event.touches);
-    //     console.log(event.targetTouches);
-    //     console.log(event.target);
+// function greeting(){
+//     console.log('Hello world');
+// }
 
-    // });
-    box.addEventListener('touchmove', function(event){
-        event.preventDefault();
-        console.log('Button touchmove ' + event.touches[0].pageX);
-    });
-    // box.addEventListener('touchend', function(event){
-    //     event.preventDefault();
-    //     console.log('Button touchend');
-    // });
+// // let timer_id = setTimeout(greeting, 5000);
+// // clearTimeout(timer_id);
+
+
+// let timer_id = setInterval(greeting, 5000);
+// clearTimeout(timer_id);
+
+// let timer_id = setTimeout(function greet(){
+//     console.log('Hello world'); 
+//     setTimeout(greet, 1000);
+
+// }); 
+let btn = document.querySelector('.btn'),
+    element = document.querySelector('.box');
+
+function move(){
+    let pos = 0;
+    let id = setInterval(frame, 10);
+
+    function frame(){
+        if (pos == 300){
+            clearInterval(id);
+        }
+        else {
+            pos++;
+            element.style.top = pos + 'px';
+            element.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', move);
+
+let btnBlock = document.querySelector('.btn-block'),
+    buttons = document.getElementsByTagName('button');
+
+btnBlock.addEventListener('click', function(event){
+    if (event.target && event.target.matches('button.first')){
+        console.log('Hello');
+    }
 });
