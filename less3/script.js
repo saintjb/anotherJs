36a09d6a7800d261109ebb7b1a9f7b593360f5e6
VIK <vikuvsh95@gmail.com>
2020-04-29 20:53:34 +0300

    added modal window

--- less3/script.js
+++ less3/script.js
@@ -1,13 +1,9 @@
-let box = document.querySelector('.box'),
-btn = document.querySelector('button');
+let obj = {
+    a:20,
+    b:15,
+    sum: function(){
+        console.log(this)
+    }
+}
 
-let width = box.offsetWidth,
-height = box.offsetHeight;
-
-console.log(width);
-console.log(height);
-console.log(box.getBoundingClientRect().left);
-console.log(document.documentElement.clientHeight);
-btn.addEventListener('click', function(){
-    console.log('Hi');
-});
+obj.sum();
